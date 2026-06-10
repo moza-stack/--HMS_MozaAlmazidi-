c37d7f9fb1d80a19bab5d37959a7f58bbf6a3b0a
Moza mohammed fadhil al-mazidi <moza.m.almazidi@gmail.com>
2026-06-10 11:45:02 +0400

    Add files via upload
     " HMS final submission"

---
+++ Hotel Management System/Hotel Management System/HotelContext.cs
@@ -0,0 +1,18 @@
+﻿using Hotel_Management_System.Models;
+
+namespace Hotel_Management_System
+{
+    public class HotelContext
+    {
+        public List<RoomModel> rooms { get; set; }
+        public List<GuestModel> guests { get; set; }
+        public List<BookingModel> bookings { get; set; } 
+        public List<ReviewModel> reviews { get; set; } 
+        public List<StaffModel> staff { get; set; } 
+
+
+        
+      }
+    }
+
+
